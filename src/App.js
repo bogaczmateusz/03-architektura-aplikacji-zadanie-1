d5a8217cbbe6d3c7ff205c13161482b60b9eab47
Mateusz Bogacz <mateusz@martcode.pl>
2020-11-01 14:04:11 +0100

    Init.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Button from './components/Button';
+
+function App() {
+    return (
+        <div className="App">
+            <Button color="clouds" bgColor="belizeHole" icon="coffee">Click me</Button>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fas);
+
+function Button(props) {
+    const { color, bgColor, icon, children } = props;
+
+    const colors = {
+        turquoise: '#1abc9c',
+        emerald: '#2ecc71',
+        peterRiver: '#3498db',
+        amethyst: '#9b59b6',
+        wetAsphalt: '#34495e',
+        greenSea: '#16a085',
+        nephritis: '#27ae60',
+        belizeHole: '#2980b9',
+        wisteria: '#8e44ad',
+        midnightBlue: '#2c3e50',
+        sunFlower: '#f1c40f',
+        carrot: '#e67e22',
+        alizarin: '#e74c3c',
+        clouds: '#ecf0f1',
+        concrete: '#95a5a6',
+        orange: '#f39c12',
+        pumpkin: '#d35400',
+        pomegranate: '#c0392b',
+        silver: '#bdc3c7',
+        asbestos: '#7f8c8d'
+    }
+
+    const styles = {
+        border: 0,
+        padding: '10px 20px',
+        color: colors[`${color}`],
+        backgroundColor: colors[`${bgColor}`]
+    }
+
+    return (
+        <button style={styles}><FontAwesomeIcon icon={['fas', icon]} /><span>{children}</span></button>
+    );
+}
+
+Button.defaultProps = {
+    bgColor: 'alizarin',
+    color: 'clouds',
+    children: 'Just Click',
+    icon: 'user'
+};
+
+export default Button;
